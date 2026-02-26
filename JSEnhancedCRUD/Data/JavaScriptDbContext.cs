bc34c40d0c7e12a5a82d3cb420e49b1eb7b52441
Justin <justinburns2008@gmail.com>
2026-02-26 10:24:48 -0800

    Added Product entity for Product table

---
+++ JSEnhancedCRUD/Data/JavaScriptDbContext.cs
@@ -0,0 +1,10 @@
+﻿using JSEnhancedCRUD.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JSEnhancedCRUD.Data;
+
+public class JavaScriptDbContext(DbContextOptions<JavaScriptDbContext> options) : IdentityDbContext(options)
+{
+    public DbSet<Product> Products { get; set; }
+}
